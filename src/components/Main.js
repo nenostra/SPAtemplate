55cf319eb24f4fd26d51d032bcc9a75f9a52d47d
cesar <cesar>
2017-03-11 16:38:08 -0800

    react-create-app on Heroku

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ export class Main extends Component {
                 <nav className="navbar navbar-default">
                     <div className="container-fluid">
                         <div className="navbar-header">
-                            <Link className="navbar-brand" to="/">Scotch Cars</Link>
+                            <Link className="navbar-brand" to="/">Page</Link>
                         </div>
                         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                             <ul className="nav navbar-nav">
